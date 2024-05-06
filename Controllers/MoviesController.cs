f2dfa0f4a05b7de4ef27248ffaa2b96865091cb6
mgomez123 <gomezmicaela123@gmail.com>
2024-05-06 13:57:56 -0300

    Cambios Semana 4

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        // GET: Movies/Details/5
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            var listMovies = new List<Movie>();
+
+            var movie1 = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror"
+            };
+            listMovies.Add(movie1);
+
+            var movie2 = new Movie
+            {
+                Genre = "Terror",
+                Id = 1,
+                Price = 1,
+                ReleaseDate = DateTime.Now,
+                Title = "La noche del terror II"
+            };
+            listMovies.Add(movie2);
+
+            return View(listMovies);
+
+        }
+    }
+}
